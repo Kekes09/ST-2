b3b23127536fddef5ba15fa0081529f2f9138955
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2026-06-01 12:22:17 +0300

    fix

--- include/tasks.h
+++ include/tasks.h
@@ -3,9 +3,8 @@
 #ifndef INCLUDE_TASKS_H_
 #define INCLUDE_TASKS_H_
 
-double solveEarthAndRope(double earth_radius_km, double added_length_m);
+double solveEarthAndRope();
 
-double solvePoolCost(double pool_radius_m, double path_width_m,
-                     double concrete_price_per_m2, double fence_price_per_m);
+double solvePoolCost();
 
 #endif  // INCLUDE_TASKS_H_
--- src/tasks.cpp
+++ src/tasks.cpp
@@ -4,27 +4,35 @@
 
 #include "circle.h"
 
-double solveEarthAndRope(double earth_radius_km, double added_length_m) {
+double solveEarthAndRope() {
+  constexpr double earth_radius_km = 6378.1;
+  constexpr double added_length_m = 1.0;
+
   double earth_radius_m = earth_radius_km * 1000.0;
 
   Circle earth(earth_radius_m);
-  Circle rope;
 
-  rope.setFerence(earth.getFerence() + added_length_m);
+  Circle new_rope;
+  new_rope.setFerence(earth.getFerence() + added_length_m);
 
-  return rope.getRadius() - earth.getRadius();
+  return new_rope.getRadius() - earth.getRadius();
 }
 
-double solvePoolCost(double pool_radius_m, double path_width_m,
-                     double concrete_price, double fence_price) {
+double solvePoolCost() {
+  constexpr double pool_radius_m = 3.0;
+  constexpr double path_width_m = 1.0;
+  constexpr double concrete_price_per_m2 = 1000.0;
+  constexpr double fence_price_per_m = 2000.0;
+
   Circle pool(pool_radius_m);
+
   Circle outer(pool_radius_m + path_width_m);
 
   double concrete_area = outer.getArea() - pool.getArea();
-  double concrete_cost = concrete_area * concrete_price;
+  double concrete_cost = concrete_area * concrete_price_per_m2;
 
   double fence_length = outer.getFerence();
-  double fence_cost = fence_length * fence_price;
+  double fence_cost = fence_length * fence_price_per_m;
 
   return concrete_cost + fence_cost;
 }
